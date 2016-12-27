a2be89d8c78d16feed61d7759b3784f934907a23
KC Lee <kckckckckckc@gmail.com>
2016-12-26 21:48:24 -0600

    [AdventOfCode] Add AdventOfCode2016 proj

---
+++ CSharpPlayground/AdventOfCode/AdventOfCode2016/Day1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.AdventOfCode2016
+{
+    class Day1
+    {
+        public static void RunDay1()
+        {
+            System.Diagnostics.Debug.WriteLine("Hi");
+
+        }
+
+    }
+}
---
+++ CSharpPlayground/AdventOfCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdventOfCode2016.Day1.RunDay1();
+        }
+    }
+}
