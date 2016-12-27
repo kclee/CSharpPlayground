0b66abd06dace5cfbe6b2423e631e9ee00f2f1a7
KC Lee <kckckckckckc@gmail.com>
2016-12-26 21:37:14 -0600

    [LeetCode] Add LeetCode Proj.

---
+++ CSharpPlayground/LeetCode/Q006ZigZagConversion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Q006ZigZagConversion
+    {
+        public static void RunQ006()
+        {
+            // convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+
+            string c = "PAYPALISHIRING";
+            int rows = 3;
+
+            System.Diagnostics.Debug.WriteLine(Convert(c, rows));
+        }
+
+        public static string Convert(string s, int numRows)
+        {
+            return "";
+        }
+    }
+}
